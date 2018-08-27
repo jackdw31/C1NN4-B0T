f233e52ef6e630e96caa1ba4e8af16994c145553
jackdw31 <42096815+jackdw31@users.noreply.github.com>
2018-08-27 05:16:07 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,9 @@ client.on("message", message => {
 
 // lenormand /////////////////////////////////
 
-var lenormand = [":horse_racing: I - Rider",":four_leaf_clover: II - Clover",":ship: III - Ship",":house: IV - House",":evergreen_tree: V - Tree",":cloud: VI - Clouds",":snake: VII - Snake",":coffin: VIII - Coffin",":bouquet: IX - Bouquet","<:scythe:483403214222524427> X - Scythe","<:whip:483404933908004885> XI - Whip",":bird: XII - Birds",":baby::skin-tone-3: XIII - Child",":fox: XIV - Fox",":bear: XV - Bear",":sparkles: XVI - Stars","<:stork:483411715740925964> XVII - Stork",":dog: XVIII - Dog","<:tower:483415061088436234> XIX - Tower","<:garden:483417653071839252> XX - Garden",":mountain_snow: XXI - Mountain","<:crossroads:483419652651876363> XXII - Crossroad",":mouse: XXIII - Mice",":heart: XXIV - Heart",":ring: XXV - Ring",":book: XXVI - Book",":envelope: XXVII - Letter",":man::skin-tone-3: XXVIII - Man",":woman::skin-tone-3: XXIX - Woman",":cherry_blossom: XXX - Lily",":sun_with_face: XXXI - Sun",":full_moon_with_face: XXXII - Moon",":key: XXXIII - Key",":fish: XXXIV - Fish",":anchor: XXXV - Anchor",":cross: XXXVI - Cross"];
+var num = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35"];
+var deck = [":horse_racing:  I - Rider",":four_leaf_clover:  II - Clover",":ship:  III - Ship",":house:  IV - House",":evergreen_tree:  V - Tree",":cloud:  VI - Clouds",":snake:  VII - Snake",":coffin:  VIII - Coffin",":bouquet:  IX - Bouquet","<:scythe:483403214222524427>  X - Scythe","<:whip:483404933908004885>  XI - Whip",":bird:  XII - Birds",":baby::skin-tone-3:  XIII - Child",":fox:  XIV - Fox",":bear:  XV - Bear",":sparkles:  XVI - Stars","<:stork:483411715740925964>  XVII - Stork",":dog:  XVIII - Dog","<:tower:483415061088436234>  XIX - Tower","<:garden:483417653071839252>  XX - Garden",":mountain_snow:  XXI - Mountain","<:crossroads:483419652651876363>  XXII - Crossroad",":mouse:  XXIII - Mice",":heart:  XXIV - Heart",":ring:  XXV - Ring",":book:  XXVI - Book",":envelope:  XXVII - Letter",":man::skin-tone-3:  XXVIII - Man",":woman::skin-tone-3:  XXIX - Woman",":cherry_blossom:  XXX - Lily",":sun_with_face:  XXXI - Sun",":full_moon_with_face:  XXXII - Moon",":key:  XXXIII - Key",":fish:  XXXIV - Fish",":anchor:  XXXV - Anchor",":cross:  XXXVI - Cross"];
+var info = ["Energy, Passion, Speed, Activity, News, Messages","Luck, Lightheartedness, Small Happinesses, Opportunity, Being Untroubled, Comedy","Departure, Farewell, Distance, Voyage, Travel, Journey, Adventure","Home, Establishment, Safety, Tradition, Custom, Privacy, Conservation","Growth, Grounded, Past Connection, Personal Growth, Spirituality, Health","Confusion, Unclarity, Misunderstanding, Insecurity, Doubt, Hidden Secrets","Desire, Seduction, Deception, Craving, Attraction, Sexuality, Wisdom, Forbidden Knowledge","Ending, Dying, Funeral, Loss, Grief, Mourning, Sadness","Flattery, Social Life, Pleasantness, Cordiality, Etiquette, Politeness, Appreciation","Accidents, Hasty Decisions, Danger, A Warning, Speed, Reckoning","Conflict, Discussions, Arguments, Debate, Scolding, Opposition, Objection","Worry, Excitement, Gossip, Chattering, Nervousness, Anxiety","New Beginnings, Child, Toddler, Play, Inexperience, Innocence, Immaturity","Selfishness, Self Care, Trickery, Suspicion, Cunning, Caution","Power, Leadership, Dominance, Influence, Short temper, Strength of character, Boss","Hope, Inspiration, Optimism, Spirituality, Dreams, Progress to Goals","Change, Transition, Movement, Recurrence, New Cycle, Yearning","Loyalty, Friendship, A Follower, Devotion, Obedience, Support","Authority, Solitude, Loneliness, Isolation, Aloofness, Ego, Arrogance","Public Affairs, Society, Culture, Teamwork, Fame, Social Networks","Difficulties, Problems, Obstacles, Impairment, Hurdles, Struggles, Challenge","Choices, Many Opportunities, Travel, Separation, Hesitation, Decisions","Dwindling, Deficiency, Depletion, Destruction, Defect, Flaw, Disease","Love, Amicability, Romanticisation, Forgiveness, Reconciliation, Softness, Charity","Commitment, Promise, Honor, Partnership, Cooperation, Cycles","Secrets, Knowledge, Education, Information, Research, Studies","Document, Email, Speech, Conversations, Expression, Information, Communication","Male in querent’s life, If the querent is male - the querent, Masculinity","Female in querent’s life, If the querent is female, the querent, Femininity","Sensuality, Sex, Virtue, Morality, Ethics, Wisdom, Parents","Happiness, Victory, Success, Power, Warmth, Truth","Subconscious, Intuition, Emotions, Fears, Desires, Fantasy","Openness, Revelation, Unlocking, Achievement, Liberation, Resolution","Finances, Business, Wealth, Values, Gain, Abundance","Stability, Restraint, Security, Resilience, Durability, Laying Foundations","Duty, Conviction, Suffering, Burden, Intolerance, Principles, Indoctrination"];
 
 function shuffle(array) {
 	var currentIndex = array.length, temporaryValue, randomIndex;
@@ -55,20 +57,26 @@ function shuffle(array) {
 
 client.on("message", message => {
 	if (message.content === PREFIX + "lenormand") {
-		shuffle(lenormand);
-		let card01 = lenormand[0];
-		let card02 = lenormand[1];
-		let card03 = lenormand[2];
+		shuffle(num);
+		let card01 = deck[num[0]];
+		let card02 = deck[num[1]];
+		let card03 = deck[num[2]];
+		let info01 = info[num[0]];
+		let info02 = info[num[1]];
+		let info03 = info[num[2]];
 		var embed = new Discord.RichEmbed()
 			.setDescription(card01)
+			.setFooter(info01)
 			.setColor(0xFBA635)
 		message.channel.sendEmbed(embed);
 		var embed = new Discord.RichEmbed()
 			.setDescription(card02)
+			.setFooter(info02)
 			.setColor(0xFBA635)
 		message.channel.sendEmbed(embed);
 		var embed = new Discord.RichEmbed()
 			.setDescription(card03)
+			.setFooter(info03)
 			.setColor(0xFBA635)
 		message.channel.sendEmbed(embed);
 	}
